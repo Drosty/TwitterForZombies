e078eeaf43e8ec36bf23181019f7fd3b678a9066
Ryan Drost <ryan.s.drost@gmail.com>
2012-10-01 14:55:20 -0500

    removed those 3 lines

--- app/controllers/zombies_controller.rb
+++ app/controllers/zombies_controller.rb
@@ -15,9 +15,6 @@ class ZombiesController < ApplicationController
   def show
     @zombie = Zombie.find(params[:id])
 
-
-
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @zombie }
